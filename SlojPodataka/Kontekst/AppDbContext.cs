0d1baf0d1e3c6430c24825bd767bfef837105f6a
D9 <stalinium325@gmail.com>
2026-07-30 19:36:36 +0200

    Kreiran AppDbContext za kontekst baze podataka

---
+++ SlojPodataka/Kontekst/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlojPodataka.Model;
+using System.Collections.Generic;
+
+namespace SlojPodataka.Kontekst
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TakmicenjeModel> TakmicenjaModelObjektiDBSet { get; set; }
+
+        public DbSet<DiplomaModel> DiplomaModelObjektiDBSet { get; set; }
+
+        public DbSet<KorisnikModel> KorisnikModelObjektiDBSet { get; set; }
+        public DbSet<UcenikModel> UcenikModelObjektiDBSet { get; set; }
+
+        public DbSet<PredmetModel> PredmetiModelObjektiDBSet { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> opcije)
+            : base(opcije)
+        {
+        }
+    }
+}
